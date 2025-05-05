3fd5b3f3a6c0fdef7610a942a1c3bf0292b50f0e
rhit-martina9 <89958644+rhit-martina9@users.noreply.github.com>
2025-05-05 19:50:54 -0400

    3d mania clone! (no collisions)

--- src/main/main.c
+++ src/main/main.c
@@ -38,7 +38,6 @@ void InitGameplayState(void);
 void UpdateGameplayState(void);
 void DrawGameplayState(void);
 void UnloadGameplayState(void);
-int FinishGameplayState(void);
 
 int fps = 60;
 typedef struct Note {
@@ -61,7 +60,7 @@ static int finishScreen = 0;
 static const int noteWidth = 50;
 static const int noteHeight = 20;
 
-static Vector3 noteSize = { .9f, .9f, .9f };
+static Vector3 noteSize = { .3f, .3f, .3f };
 
 static Lane* lanes;
 static const int numNotes = 20;
@@ -94,39 +93,37 @@ void InitGameplayState(void)
 {
 //    // TODO: Initialize GAMEPLAY screen variables here!
     framesCounter = 0;
-//    pause = true;
-//
-//    // basic calculation on note distance per beat
-//    noteGap = noteSpeed * fps * 60 / bpm;
-//
-//    // init lanes
-//    lanes = malloc(sizeof(Lane) * numLanes);
-//
-//    for (int i = 0; i < numLanes; i++) {
-//        lanes[i].notes = malloc(sizeof(Note) * numNotes);
-//        lanes[i].nextNote = 0;
-//        lanes[i].numNotes = 0;
-//        lanes[i].hasHeldNote = false;
-//    }
-//
-//    for (int i = 0; i < numNotes; i++) {
-//        int lane = rand() % numLanes;
-//        if (rand() % chanceHold == 0) {
-//            lanes[lane].notes[lanes[lane].numNotes] = (Note){ (Vector3) { -(noteGap * i + 5.0f),0.0f,lane}, BLUE, 1};
-//        }
-//        else {
-//            lanes[lane].notes[lanes[lane].numNotes] = (Note){ (Vector3) { -(noteGap * i + 5.0f),0.0f,lane}, RED, 0};
-//            lanes[lane].numNotes++;
-//            if (rand() % chanceHalf == 0) {
-//                lane = rand() % numLanes;
-//                lanes[lane].notes[lanes[lane].numNotes] = (Note){ (Vector3) { -(noteGap * (i + .5f) + 5.0f),0.0f,lane}, RED, 0};
-//            }
-//        }
-//        lanes[lane].numNotes++;
-//        // laneD[i] = (Note) {0, (int) (noteGap * (i + 5)), RED};
-//    }
+    pause = false;
 
-    note = (Note){ (Vector3) { -5.0f,1.0f,-1.0f}, RED, 0};
+    // basic calculation on note distance per beat
+    noteGap = noteSpeed * fps * 60 / bpm;
+
+    // init lanes
+    lanes = malloc(sizeof(Lane) * numLanes);
+
+    for (int i = 0; i < numLanes; i++) {
+        lanes[i].notes = malloc(sizeof(Note) * numNotes);
+        lanes[i].nextNote = 0;
+        lanes[i].numNotes = 0;
+        lanes[i].hasHeldNote = false;
+    }
+
+    for (int i = 0; i < numNotes; i++) {
+        int lane = rand() % numLanes;
+        if (rand() % chanceHold == 0) {
+            lanes[lane].notes[lanes[lane].numNotes] = (Note){ (Vector3) { lane*2-3.5,0.75f,-(noteGap * i + 5.0f)}, BLUE, 1};
+        }
+        else {
+            lanes[lane].notes[lanes[lane].numNotes] = (Note){ (Vector3) { lane*2-3.5,0.75f,-(noteGap * i + 5.0f)}, RED, 0};
+            lanes[lane].numNotes++;
+            if (rand() % chanceHalf == 0) {
+                lane = rand() % numLanes;
+                lanes[lane].notes[lanes[lane].numNotes] = (Note){ (Vector3) { lane*2-3.5,0.75f,-(noteGap * (i + .5f) + 5.0f)}, RED, 0};
+            }
+        }
+        lanes[lane].numNotes++;
+        // laneD[i] = (Note) {0, (int) (noteGap * (i + 5)), RED};
+    }
 }
 
 void UpdateGameplayState() {
@@ -141,26 +138,79 @@ void UpdateGameplayState() {
     if (IsVRButtonPressed(3)) {
         setVRControllerVibration(1, 3000, 0.5, -1);
     }
-    note.position.x += 0.01;
+
+    for (int i = 0; i < numLanes; i++) {
+        while (lanes[i].notes[lanes[i].nextNote].position.z > 10.0f) {
+            lanes[i].nextNote++;
+            printf("skipped a note at lane %d\n", i);
+        }
+    }
+
+    if (!pause) {
+        for (int i = 0; i < numLanes; i++) {
+            for (int j = lanes[i].nextNote; j < lanes[i].numNotes; j++) {
+                lanes[i].notes[j].position.z += noteSpeed;
+            }
+            if (lanes[i].hasHeldNote) {
+                lanes[i].heldNote.position.z += noteSpeed;
+            }
+        }
+    }
 }
 
 void DrawGameplayState() {
-    DrawVRBackground(selfLoc.x, selfLoc.z); // this draws the 2d wallpaper stretched across a curved rectangle encompassing roughly 120 degrees
+//    DrawVRBackground(selfLoc.x, selfLoc.z); // this draws the 2d wallpaper stretched across a curved rectangle encompassing roughly 120 degrees
 //    DrawVRCube((Vector3){1.0f, 2.0f, 3.0f}, 1.0f, (Color){19 ,37, 207, 255});
 //    DrawVRCube(note.position,noteSize.x,note.color);
+    Vector4 o0 = {0.0f,0.0f,0.0f,1.0f};
     for (int eye = 0; eye < 2; eye++) {
         BeginVRDraw(eye);
-        DrawVRCuboid((Vector3){0.2f, 0.0f, -1.0f}, (Vector4){0.0f,0,0,1}, (Vector3){0.1f, 0.1f, 0.1f}, (Color){127,255,255,0});
-        DrawVRCuboid((Vector3){1.2f, 0.0f, -1.0f}, (Vector4){0.0f,0,0,1}, (Vector3){0.1f, 0.1f, 0.1f}, (Color){127,255,255,0});
-        DrawVRCuboid((Vector3){0.2f, 1.0f, -1.0f}, (Vector4){0.0f,0,0,1}, (Vector3){0.03f, 0.03f, 0.03f}, (Color){127,255,255,0});
+//        DrawVRCuboid((Vector3){0.2f, 0.0f, -1.0f}, o0, (Vector3){0.1f, 0.1f, 0.1f}, (Color){127,255,255,0});
+//        DrawVRCuboid((Vector3){1.2f, 0.0f, -1.0f}, o0, (Vector3){0.1f, 0.1f, 0.1f}, (Color){127,255,255,0});
+//        DrawVRCuboid((Vector3){0.2f, 1.0f, -1.0f}, o0, (Vector3){0.03f, 0.03f, 0.03f}, (Color){127,255,255,0});
 
-        DrawVRCuboid(note.position, (Vector4){0.0f,0,0,1}, (Vector3){0.03f,0.03f,0.03f}, (Color){127,255,255,0});
-//        Vector3 basePos = GetControllerPosition(2);
-//        Vector4 baseOrientation = GetControllerOrientation(2);
+        DrawVRCuboid(note.position, o0, (Vector3){0.03f,0.03f,0.03f}, (Color){127,255,255,0});
+        Vector3 lPos = GetControllerPosition(0);
+        Vector4 lOrientation = GetControllerOrientation(0);
+        DrawVRCuboid(lPos, lOrientation, (Vector3){0.03f,0.03f,0.03f}, (Color){127,255,255,0});
+        Vector3 rPos = GetControllerPosition(1);
+        Vector4 rOrientation = GetControllerOrientation(1);
+        DrawVRCuboid(rPos, rOrientation, (Vector3){0.03f,0.03f,0.03f}, (Color){127,255,255,0});
 //        for (int i = 0; i < 20; i++) {
 //            Vector3 pos = {basePos.x, basePos.y+i, basePos.z};
 //            DrawVRCuboid(pos, (Vector4){0.0f,0,0,1}, (Vector3){0.03f,0.03f,0.03f}, (Color){127,255,255,0} );
 //        }
+
+        for (int i = 0; i < numLanes; i++) {
+            for (int j = lanes[i].nextNote; j < lanes[i].numNotes; j++) {
+                if (lanes[i].notes[j].holdLength && !lanes[i].hasHeldNote) {
+                    int newZ = lanes[i].notes[j].position.z - noteGap * lanes[i].notes[j].holdLength / 2;
+                    Vector3 newPos = { lanes[i].notes[j].position.y, lanes[i].notes[j].position.z, newZ};
+                    DrawVRCuboid(newPos,o0, (Vector3){noteSize.x, noteSize.y,noteGap * lanes[i].notes[j].holdLength +  noteSize.z}, lanes[i].notes[j].color);
+                }
+                else {
+                    DrawVRCuboid(lanes[i].notes[j].position,o0, noteSize, lanes[i].notes[j].color);
+                }
+            }
+            if (lanes[i].hasHeldNote) {
+                int newZ = lanes[i].heldNote.position.z - noteGap * lanes[i].heldNote.holdLength / 2;
+                Vector3 newPos = {lanes[i].heldNote.position.x, lanes[i].heldNote.position.y, newZ};
+                DrawVRCuboid(newPos, o0, (Vector3){noteSize.x, noteSize.y, noteGap * lanes[i].heldNote.holdLength + noteSize.z}, lanes[i].heldNote.color);
+            }
+        }
+
+        // DrawGrid(40, 1.0f);
+//        for (int i = 0; i < 4; i++) DrawVRCuboid((Vector3){0.0f+i, 0.0f, 0.0f},o0,(Vector3){0.05f,0.05f,20.0f}, GRAY);
+
         EndVRDraw(eye);
     }
+}
+
+void UnloadGameplayState(void)
+{
+    // TODO: Unload GAMEPLAY screen variables here!
+    for (int i = 0; i < numLanes; i++) {
+        free(lanes[i].notes);
+    }
+    free(lanes);
 }
